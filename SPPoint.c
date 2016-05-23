bd44d4f965f248bdabcb92cb2893250cac0cf06c
elisheva <elisheva>
2016-05-23 17:50:49 +0300

    add sPPoint tests

--- SPPoint.c
+++ SPPoint.c
@@ -10,8 +10,8 @@
 #include <assert.h>
 
 //TODO: find out about creating and destroying logger
-//TODO: is it OK to always use -> ?
-//TODO: Placement of assert - asked in forumm
+
+
 
 
 struct sp_point_t{
