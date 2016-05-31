115c25471892a8146d7f0018fc18aaec704e1075
elisheva <elisheva>
2016-05-31 18:16:20 +0300

    remove logger from SPPoint.c

--- SPPoint.c
+++ SPPoint.c
@@ -6,11 +6,7 @@
  */
 #include "SPPoint.h"
 #include <stdlib.h>
-#include "SPLogger.h"
 #include <assert.h>
-
-//TODO: find out about creating and destroying logger
-
 
 
 
@@ -25,40 +21,28 @@ struct sp_point_t{
 		SPPoint point;
 		double* coorData;
 		int i;
-		SP_LOGGER_MSG logmsg;
-		const char* logFile = NULL;
 
 
-		// initialize logger
-		logmsg = spLoggerCreate(logFile, SP_LOGGER_DEBUG_INFO_WARNING_ERROR_LEVEL);
-		if ((logmsg == SP_LOGGER_OUT_OF_MEMORY) || (logmsg == SP_LOGGER_CANNOT_OPEN_FILE)) {
-			return NULL;
-		}
 
 		// check validation of parameters
 		if (data == NULL) {
-			spLoggerPrintError("cannot create point with NULL data", __FILE__, __func__, __LINE__);
 			return NULL;
 		}
 		if (dim <= 0) {
-			spLoggerPrintError("cannot create point with negative or zero dimension", __FILE__, __func__, __LINE__);
 			return NULL;
 		}
 		if (index < 0) {
-			spLoggerPrintError("cannot create point with negative index", __FILE__, __func__, __LINE__);
 			return NULL;
 		}
 
 		point = (SPPoint)malloc(sizeof(*point));
 		if (point == NULL){
-			spLoggerPrintError("error allocating new point", __FILE__, __func__, __LINE__);
 			return NULL;
 		}
 
 		// allocate memory for coorData
 		coorData = (double*)malloc(dim*sizeof(double));
 		if (coorData == NULL){
-			spLoggerPrintError("ALLOCATION ERRorr", __FILE__, __func__, __LINE__);
 			return NULL;
 		}
 
@@ -83,8 +67,6 @@ struct sp_point_t{
 
 		copiedPoint = spPointCreate(source->coordinates, source->dim, source->index);
 		if (copiedPoint==NULL){
-			//since source is a valid point the only error that can accrue during the call to spPointCreate is an allocation error
-			spLoggerPrintError("ALLOCATION ERRorr", __FILE__, __func__, __LINE__);
 			return NULL;
 		}
 		return copiedPoint;
